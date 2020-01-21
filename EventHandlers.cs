2dd175eeae0a7eb937fbf20a326506be9aff3b4d
Nexobeta28 <alvaroesparza2@gmail.com>
2020-01-21 21:19:11 +0100

    First commit

---
+++ EventHandlers.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using EXILED;
+using Grenades;
+using MEC;
+
+namespace WelcomeBroadcast
+{
+	public class EventHandlers
+	{
+        //BROADCAST VARS
+        uint time = 5;
+        string message = "Welcome to my server!";
+
+        public Plugin plugin;
+		public EventHandlers(Plugin plugin) => this.plugin = plugin;
+
+		public void OnPlayerJoin(PlayerJoinEvent ev)
+		{
+			ev.Player.Broadcast(time, message);
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+using System;
+using EXILED;
+
+namespace WelcomeBroadcast
+{
+	public class Plugin : EXILED.Plugin
+	{
+		//Instance variable for eventhandlers
+		public EventHandlers EventHandlers;
+		
+		public override void OnEnable()
+		{
+			try
+			{
+                Plugin.Info("WelcomeBroadcast plugin loading...");
+				EventHandlers = new EventHandlers(this);
+
+				Events.PlayerJoinEvent += EventHandlers.OnPlayerJoin;
+                Plugin.Info("WelcomeBroadcast plugin loaded!");
+			}
+			catch (Exception e)
+			{
+				Error($"WelcomeBroadcast plugin error: {e}");
+			}
+		}
+
+		public override void OnDisable()
+		{
+			EventHandlers = null;
+		}
+
+		public override void OnReload()
+		{
+			
+		}
+
+		public override string getName { get; } = "WelcomeBroadcast";
+	}
+}
